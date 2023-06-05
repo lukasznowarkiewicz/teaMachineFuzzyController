362bd96b9e4f8f9c084112431e1ab64e08823a78
lukasznowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-06-05 19:28:32 +0200

    Updated paths

--- shell_scripts/run_app.sh
+++ shell_scripts/run_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigacja do lokalizacji, gdzie jest repozytorium
-cd ~/repo/python_desktop_app
+cd ~/repo/iss_proj
 
 # Pobieranie aktualizacji z repozytorium GitHub
 git pull
@@ -9,7 +9,8 @@ git pull
 # Otwieranie przeglądarki Chromium w trybie kiosku
 # Opcje --noerrdialogs, --disable-infobars i --check-for-update-interval=31536000
 # służą do wyłączenia dialogów błędów, pasków informacyjnych i aktualizacji
-chromium-browser --kiosk 'http://localhost:8050/' --noerrdialogs --disable-infobars --check-for-update-interval=31536000 &
+chromium-browser --kiosk 'file://~/repo/iss_proj/web_app/index.html' --noerrdialogs --disable-infobars --check-for-update-interval=31536000 &
 
 # Uruchomienie aplikacji main.py
-python3 main.py &
+python3 python_desktop_app/main.py &
+
