8a2e27180b14abdb09d8407ba4ed8f495c07562b
Dorota Agnieszka Solarska <69536773+Morsdors@users.noreply.github.com>
2023-06-08 16:04:05 +0200

    add temperature set to the web interface

--- python_desktop_app/main.py
+++ python_desktop_app/main.py
@@ -1,19 +1,21 @@
 import time
 import serial
-import serial.tools.list_ports
+from serial.tools import list_ports
 import plotly.graph_objects as go
 from dash import Dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Output, Input
 import collections
 import pandas as pd
 
 # Wyszukaj urządzenia podłączone do komputera przez port szeregowy
-ports = serial.tools.list_ports.comports()
+ports = serial.tools.list_ports.comports() #serial.tools.list_ports.comports()
 device = None
 
+
 for port in ports:
+    #print("port", port)
     if 'Pico' in port.description:
         device = port.device
         break
@@ -25,8 +27,8 @@ else:
 
 ser = serial.Serial(device, 9600)
 
-commands = ['H1-ON', 'H2-ON', 'H3-ON', 'P1-ON', 'H1-OFF', 'H2-OFF', 'H3-OFF', 'P1-OFF']
-
+commands = ['H1-ON', 'H2-ON', 'H3-ON', 'P1-ON', 'H1-OFF', 'H2-OFF', 'H3-OFF', 'P1-OFF', 'T0-?', 'T1-?', 'T2-?', 'T3-?']
+temperatures = list(range(65, 101, 5))
 # Queue to store history of data
 data_queue = collections.deque(maxlen=120*2)  # store data for last 2 minutes (120s * 2 per second)
 
@@ -38,6 +40,7 @@ def send_command(command):
     data_queue.append((pd.Timestamp.now(), command.split('-')[0], 100 if 'ON' in response else 0))
 
 app = Dash(__name__)
+
 
 app.layout = html.Div(
     children=[
@@ -65,11 +68,19 @@ app.layout = html.Div(
                 'marginBottom': '30px'
             }
         ),
+        html.Label('Wybierz temperaturę, do której chcesz podgrzać wodę:'),
+        dcc.Dropdown(
+            id='temperature-dropdown',
+            options=[{'label': str(temp), 'value': temp} for temp in temperatures],
+            value=temperatures[0]
+        ),
+        html.Div(id='selected-temperature'),
         dcc.Graph(id="live-chart", style={'height': '80vh', 'width': '100%'}),
         dcc.Interval(id="interval-component", interval=1000, n_intervals=0),
     ],
     style={'width': '100%', 'height': '100vh', 'margin': 'auto'}
 )
+
 
 @app.callback(Output("toggle-button", "children"), Input("toggle-button", "n_clicks"))
 def toggle_devices(n_clicks):
@@ -78,9 +89,13 @@ def toggle_devices(n_clicks):
             send_command(command)
         return 'Wyłącz wszystko'
     else:
-        for command in commands[4:]:
+        for command in commands[4:8]:
             send_command(command)
         return 'Włącz wszystko'
+
+@app.callback(Output('selected-temperature', 'children'), Input('temperature-dropdown', 'value'))
+def update_selected_temperature(selected_temperature):
+    return html.H3(f'Wybrana temperatura: {selected_temperature}')
 
 @app.callback(Output("live-chart", "figure"), Input("interval-component", "n_intervals"))
 def update_graph(n_intervals):
